c56b8ba6fe8f0bd40121dd08f0d9d9b2d4968c4f
Trung Nguyen <trungnguyen@Trungs-MacBook-Pro.local>
2022-04-04 02:41:45 -0400

    add forecast

--- Assets/JS/Weather.js
+++ Assets/JS/Weather.js
@@ -1,6 +1,6 @@
 var searchBtn=document.getElementById("search-btn");
 var inputValue=document.getElementById("cityname");
-const baseUrl= "https://api.openweathermap.org/data/2.5/weather?q=";
+const baseUrl= "https://api.openweathermap.org/data/2.5/forecast?q=";
 const apiKey= "7406347c68cf592e6562563f43b31ea4";
 var cityName= document.getElementById('city-name');
 var weather=document.getElementById('weather');
@@ -9,9 +9,13 @@ var humidity=document.getElementById('humidity');
 var wind=document.getElementById('wind');
 
 var displayWeather=document.getElementById('display-weather');
+var day1=document.getElementById("1");
+var day2=document.getElementById("2");
+var day3=document.getElementById("3");
+var day4=document.getElementById("4");
+var day5=document.getElementById("5");
 
-var baseWeatherUrl="http://openweathermap.org/img/wn/"
-var weatherTail="@2x.png"
+
 
 
 
@@ -23,51 +27,69 @@ searchBtn.addEventListener('click',function(){
 .then(data=>{  
 
     console.log(data);
-    var timestamp =data.dt *1000;
+    var timestamp =data.list[0].dt *1000;
     var date = new Date(timestamp);
     // weather.=data.weather;
-    temp.innerHTML="Temp: "+ Math.floor((parseInt(data.main.temp)-272.15)).toString() + "°C";
-    humidity.innerHTML="Humidity: " + data.main.humidity
-    wind.innerHTML="Wind: "+data.wind.speed;
-    cityName.innerHTML=data.name + " "+date;
+    temp.innerHTML="Temp: "+ Math.floor((parseInt(data.list[0].main.temp)-272.15)).toString() + "°C";
+    humidity.innerHTML="Humidity: " + data.list[0].main.humidity
+    wind.innerHTML="Wind: "+data.list[0].wind.speed;
+    cityName.innerHTML=data.city.name + " "+date;
+
+
+    // day1.innerHTML=data.list[0].weather.icon[0];
+// day2.innerHTML=data.list[1].weather.icon[1];
+// day3.innerHTML=data.list[2].weather.icon[2];
+// day4.innerHTML=data.list[3].weather.icon[3];
+// day5.innerHTML=data.list[4].weather.icon[4];
+
+for(i=1;i<=5;i++){
+    var card= document.createElement('div');
+    var dateForecast=document.createElement('p');
+    var tempForecast=document.createElement('p');
+    var iconForecast=document.createElement('img');
+    var humidityForecast=document.createElement('p');
+    var windForecast=document.createElement('p');
+    var iconCode=data.list[i].weather[0].icon;
+    var iconURL= "http://openweathermap.org/img/wn/"+iconCode+"@2x.png";
+
+    card.appendChild(dateForecast);
+    card.appendChild(iconForecast);
+    card.appendChild(tempForecast);
+    card.appendChild(humidityForecast);
+    card.appendChild(windForecast);
+
+    iconForecast.setAttribute('src',iconURL);
+    tempForecast.innerHTML="Temp: "+ Math.floor((parseInt(data.list[0].main.temp)-272.15)).toString() + "°C";
+    humidityForecast.innerHTML="Humidity: " + data.list[0].main.humidity
+    windForecast.innerHTML="Wind: "+data.list[0].wind.speed;
+    dateForecast.innerHTML= new Date (data.list[i].dt *1000);
 
     
     
+
+
+
+    displayWeather.appendChild(card);
+  
+
+}
+  
 
     
 })
 
 .catch(err=>console.log(err))
 
-})
+});
 
 
 
 
-var weatherCode= data.icon;
-var day1=document.getElementById("1");
-var day2=document.getElementById("2");
-var day3=document.getElementById("3");
-var day4=document.getElementById("4");
-var day5=document.getElementById("5");
 
 
-fetch(`${baseWeatherUrl}${weatherCode}${weatherTail}`)
-.then(resposne=>resposne.json())
-.then(data=>{  
 
-   day1.innerHTML=weatherCode;
-   day2.innerHTML=weatherCode.
 
-    
-    
 
-    
-})
-
-.catch(err=>console.log(err))
-
-})
 
 
 
--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 I created a page which displays all the infomation followed by mocking-up photo.
 After that, Fetch openweather API 5 days to get the data and pull it into the code.
-
+However, I figured out I can only get current date information. It meant I do not have list section in the array so I could not get the 5 days forecast properly.
 
 * Github link: https://github.com/trungbr007/Weather-Dasboard-Week-6-ServerSide-API
 * Deployed link: https://trungbr007.github.io/Weather-Dasboard-Week-6-ServerSide-API/
