66e69208f999c154ccc45e0e6c3e4f9a46ec7d85
Trung Nguyen <trungnguyen@Trungs-MBP.ht.home>
2022-04-04 00:43:53 -0400

    add icons

--- Assets/JS/Weather.js
+++ Assets/JS/Weather.js
@@ -7,8 +7,12 @@ var weather=document.getElementById('weather');
 var temp=document.getElementById('temp');
 var humidity=document.getElementById('humidity');
 var wind=document.getElementById('wind');
-var timestamp = 1648961485000
-var date = new Date(timestamp);
+
+var displayWeather=document.getElementById('display-weather');
+
+var baseWeatherUrl="http://openweathermap.org/img/wn/"
+var weatherTail="@2x.png"
+
 
 
 
@@ -19,12 +23,43 @@ searchBtn.addEventListener('click',function(){
 .then(data=>{  
 
     console.log(data);
-
+    var timestamp =data.dt *1000;
+    var date = new Date(timestamp);
     // weather.=data.weather;
     temp.innerHTML="Temp: "+ Math.floor((parseInt(data.main.temp)-272.15)).toString() + "°C";
     humidity.innerHTML="Humidity: " + data.main.humidity
     wind.innerHTML="Wind: "+data.wind.speed;
-    cityName.innerHTML=data.name;
+    cityName.innerHTML=data.name + " "+date;
+
+    
+    
+
+    
+})
+
+.catch(err=>console.log(err))
+
+})
+
+
+
+
+var weatherCode= data.icon;
+var day1=document.getElementById("1");
+var day2=document.getElementById("2");
+var day3=document.getElementById("3");
+var day4=document.getElementById("4");
+var day5=document.getElementById("5");
+
+
+fetch(`${baseWeatherUrl}${weatherCode}${weatherTail}`)
+.then(resposne=>resposne.json())
+.then(data=>{  
+
+   day1.innerHTML=weatherCode;
+   day2.innerHTML=weatherCode.
+
+    
     
 
     
--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # Weather-Dasboard-Week-6-ServerSide-API
+
+I created a page which displays all the infomation followed by mocking-up photo.
+After that, Fetch openweather API 5 days to get the data and pull it into the code.
+
+
+* Github link: https://github.com/trungbr007/Weather-Dasboard-Week-6-ServerSide-API
+* Deployed link: https://trungbr007.github.io/Weather-Dasboard-Week-6-ServerSide-API/
+
+
+<img src="./Screenshot.png">
+
